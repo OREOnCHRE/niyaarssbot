48fbe6dff000c455af7f7e99219935d09bb13d7c
oreo <112804412+OREOnCHRE@users.noreply.github.com>
2022-09-04 11:40:11 +0000

    Update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-DATABASE_URL = os.environ.get("https://dashboard.heroku.com/apps/rssfedd")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL)
